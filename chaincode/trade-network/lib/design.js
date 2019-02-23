fa1970d1deb6788cafa8bb943b81551b99be7b6a
ivangsp <ojiamboivan@gmail.com>
2019-02-23 11:14:37 +0200

    add create product design

---
+++ chaincode/trade-network/lib/design.js
@@ -0,0 +1,55 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-tabs */
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+/* global getFactory getAssetRegistry getParticipantRegistry emit query */
+const namespace = 'org.trade.com';
+
+
+/**
+ *
+ * @param {org.trade.com.CreateProductDesign} designRequest - the createProductDesign transaction
+ * @transaction
+ */
+async function createProductDesign(designRequest) {
+    const factory = getFactory();
+
+    const id = 'D#' + new Date().getTime().toString();
+    const design = factory.newResource(namespace, 'ProductDesign', );
+    design.material = designRequest.material;
+    design.specification = designRequest.specification;
+    const designers = [];
+    designRequest.designers.forEach(designer => {
+        designers.push(factory.newRelationship(namespace, 'User', designer.getIdentifier()));
+    });
+    if (designers.length === 0) {
+        // eslint-disable-next-line no-throw-literal
+        throw 'Please enter a valid designers id';
+    }
+
+    design.designers = designers;
+    design.dateCreated = new Date();
+    design.name = designRequest.name;
+
+    // save the product design
+    const assetRegistry = await getAssetRegistry(design.getFullyQualifiedType());
+    await assetRegistry.add(design);
+
+    // emit event
+    // const deisgnInfo = factory.newEvent(namespace, 'CreateProductDesignEvent');
+    // emit(deisgnInfo);
+}
